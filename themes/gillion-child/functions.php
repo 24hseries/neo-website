6a98358a6e0f748e20e0b7c4f5c347f950db429d
Niel Hekkens <nielhekkens@gmail.com>
2018-12-24 18:15:16 +0100

    Change some comments

--- themes/gillion-child/functions.php
+++ themes/gillion-child/functions.php
@@ -25,7 +25,9 @@ add_action('wp_enqueue_scripts', 'loadPQEntryList');
 add_action('wp_enqueue_scripts', 'loadStandings');
 add_action('wp_enqueue_scripts', 'loadResults');
 
-// Load entry list
+/**
+ * Load entry list JavaScript file on the right page.
+ */
 function loadEntryList(){
 	if(is_page('769')){
 		wp_register_script('show-registrations', get_stylesheet_directory_uri().'/js/nes5_entrylist.js', array('jquery'), null, true);
@@ -33,7 +35,9 @@ function loadEntryList(){
 	}
 }
 
-// Load pre-qualifying entry list
+/**
+ * Load pre-qualifying entry list JavaScript file on the right page.
+ */
 function loadPQEntryList(){
 	if(is_page('2875')){
 		wp_register_script('show-pq-registrations', get_stylesheet_directory_uri().'/js/nes5_pq_entrylist.js', array('jquery'), null, true);
@@ -41,14 +45,18 @@ function loadPQEntryList(){
 	}
 }
 
-// Load standings
+/**
+ * Load standings JavaScript file on the right page.
+ */
 function loadStandings(){
 	if(is_page('1072')){
 		wp_enqueue_script('show-standings', get_stylesheet_directory_uri().'/js/show_standings.js', array('jquery'), null, true);
 	}
 }
 
-// Load results
+/**
+ * Load JavaScript file that loads the results on the right page.
+ */
 function loadResults(){
 	if(is_page('1090')){
 		wp_enqueue_script('show-results', get_stylesheet_directory_uri().'/js/show_results.js', array('jquery'), null, true);
