d56031c04f7e8479c3eec064567eb420c5157467
Niel Hekkens <hello@nielhekkens.nl>
2021-08-13 14:09:58 +0200

    Update url for season 21-22

--- php/entry-list/24h-entrylist.php
+++ php/entry-list/24h-entrylist.php
@@ -3,7 +3,7 @@ header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
  
 // Set your CSV feed
-$feed = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQYNRY2kdn6DuKrrZTg5vZrqTLEXLwofIKjAaEqnP21JLAcWRXvhvImD91DvBHZ75cgIP2_ztZ2wJW3/pub?gid=322623071&single=true&output=csv";
+$feed = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSm3YXxdB1Sy5jOMXDS_LSQFU0HI1jItmJ3LN4zB93s4LbL16ZDX8Ylr6rrKWZNKBY96EfP_px1HfDy/pub?gid=723814366&single=true&output=csv";
  
 // Arrays we'll use later
 $keys = array();
